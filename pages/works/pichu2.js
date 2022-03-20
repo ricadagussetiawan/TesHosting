47b65cc0240a8a93307dbe53481a54abe402aa29
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-03-20 10:41:17 +0700

    Update pichu2.js

--- pages/works/pichu2.js
+++ pages/works/pichu2.js
@@ -9,7 +9,7 @@ import {
   Center
 } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
+//import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 
