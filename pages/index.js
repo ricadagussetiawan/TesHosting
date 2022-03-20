bcb0746e7d57053777c3e1c77b93c468742e06d3
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-03-20 10:48:54 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,223 +1,223 @@
 import NextLink from 'next/link'
 import {
-  Link,
-  Container,
-  Heading,
-  Box,
-  SimpleGrid,
   Button,
+  Container,
+  Box,
+  Heading,
+  Image,
   List,
-  ListItem,
+  Link,
   useColorModeValue,
-  chakra
+  Icon,
+  ListItem
 } from '@chakra-ui/react'
+import Section from '../components/section.js'
+import Paragraph from '../components/paragraph.js'
 import { ChevronRightIcon } from '@chakra-ui/icons'
-import Paragraph from '../components/paragraph'
+import Layout from '../components/layouts/article.js'
+import {
+  IoLogoWhatsapp,
+  IoLogoGithub,
+  IoLogoTiktok,
+  IoLogoPlaystation,
+  IoLogoPinterest
+} from 'react-icons/io5'
 import { BioSection, BioYear } from '../components/bio'
-import Layout from '../components/layouts/article'
-import Section from '../components/section'
-import { GridItem } from '../components/grid-item'
-import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
-import thumbYouTube from '../public/images/links/youtube.png'
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
-import Image from 'next/image'
 
-const ProfileImage = chakra(Image, {
-  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
-})
-
-const Home = () => (
-  <Layout>
-    <Container>
-      <Box
-        borderRadius="lg"
-        mb={6}
-        p={3}
-        textAlign="center"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-      >
-        Hello, I&apos;m a full-stack developer based in Japan!
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Takuya Matsuyama
-          </Heading>
-          <p>Digital Craftsman ( Artist / Developer / Designer )</p>
-        </Box>
+function Page() {
+  return (
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
+          align="center"
         >
+          Im From Indonesian , In DKI Jakarta
+        </Box>
+
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-tittle">
+              Ricad Agus Setiawan
+            </Heading>
+            <p>( Developer / Designer UI/UX )</p>
+          </Box>
           <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow='hidden'
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
           >
-            <ProfileImage
-              src="/images/takuya.jpg"
-              alt="Profile image"
-              borderRadius='full'
-              width="100%"
-              height="100%"
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/DSC_0053 copy-min-min.jpg"
+              alt="Profile Image"
             />
           </Box>
         </Box>
-      </Box>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-tittle">
+            Work
+          </Heading>
+          <Paragraph>
+            Ricad is a freelance and a full-stack developer based in Osaka with
+            a passion for building digital services/stuff he wants. He has a
+            knack for all things launching products, from planning and designing
+            all the way to solving real-life problems with code. When not
+            online, he loves hanging out with his camera. Currently, he is
+            living off of his own product called{' '}
+            <NextLink href="/works/inkdrop">
+              <Link>Inkdrop</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
 
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          Takuya is a freelance and a full-stack developer based in Osaka with a
-          passion for building digital services/stuff he wants. He has a knack
-          for all things launching products, from planning and designing all the
-          way to solving real-life problems with code. When not online, he loves
-          hanging out with his camera. Currently, he is living off of his own
-          product called{' '}
-          <NextLink href="/works/inkdrop" scroll={false}>
-            <Link>Inkdrop</Link>
-          </NextLink>
-          .
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works" scroll={false}>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My portfolio
-            </Button>
-          </NextLink>
-        </Box>
-      </Section>
-
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1984</BioYear>
-          Born in Osaka (大阪), Japan.
-        </BioSection>
-        <BioSection>
-          <BioYear>2010</BioYear>
-          Completed the Master&apos;s Program in the Graduate School of
-          Information Science at Nara Institute of Science and Technology
-          (奈良先端科学技術大学院大学情報科学研究科修士課程)
-        </BioSection>
-        <BioSection>
-          <BioYear>2010</BioYear>
-          Worked at Yahoo! Japan (ヤフー株式会社入社)
-        </BioSection>
-        <BioSection>
-          <BioYear>2012 to present</BioYear>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-tittle">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1997</BioYear>
+            Born in Jakarta , Indonesia .
+          </BioSection>
+          <BioSection>
+            <BioYear>2018</BioYear>
+            Worked at Yahoo! Japan (ヤフー株式会社入社)
+          </BioSection>
+          <BioSection>
+            <BioYear>2019 to present</BioYear>
+          </BioSection>
+          <BioSection>
+            <BioYear>2018</BioYear>
+            Completed the Master&apos;s Program in the Graduate at UI faculty of
+            international law
+          </BioSection>
+          <BioSection>
+            <BioYear>2022</BioYear>
+            Completed the Master&apos;s Program in the Graduate School Of
+            informatics Science at STIE & STMIK Jayakarta
+          </BioSection>
           Works as a freelance
-        </BioSection>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>
-          Art, Music,{' '}
-          <Link href="https://illust.odoruinu.net/" target="_blank">
-            Drawing
-          </Link>
-          , Playing Drums,{' '}
-          <Link href="https://500px.com/p/craftzdog" target="_blank">
-            Photography
-          </Link>
-          , Leica, Machine Learning
-        </Paragraph>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          On the web
-        </Heading>
-        <List>
-          <ListItem>
-            <Link href="https://github.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoGithub />}
-              >
-                @craftzdog
-              </Button>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>
+            Art, Music,{' '}
+            <Link href="" target="_blank">
+              Drawing
             </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/inkdrop_app" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoTwitter />}
-              >
-                @inkdrop_app (English)
-              </Button>
+            , Playing Drums,{' '}
+            <Link href="" target="_blank">
+              Photography
             </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://twitter.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoTwitter />}
-              >
-                @craftzdog (日本語)
-              </Button>
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link href="https://instagram.com/craftzdog" target="_blank">
-              <Button
-                variant="ghost"
-                colorScheme="teal"
-                leftIcon={<IoLogoInstagram />}
-              >
-                @craftzdog
-              </Button>
-            </Link>
-          </ListItem>
-        </List>
+            , Google Machine Learning
+          </Paragraph>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-tittle">
+            Social Media
+          </Heading>
+          <List>
+            <ListItem>
+              <Link href="https://github.com/icat108" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoGithub} />}
+                >
+                  @icat108
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
 
-        <SimpleGrid columns={[1, 2, 2]} gap={6}>
-          <GridItem
-            href="https://www.youtube.com/devaslife"
-            title="Dev as Life"
-            thumbnail={thumbYouTube}
-          >
-            My YouTube channel
-          </GridItem>
-          <GridItem
-            href="https://www.inkdrop.app/"
-            title="Inkdrop"
-            thumbnail={thumbInkdrop}
-          >
-            A Markdown note-taking app
-          </GridItem>
-        </SimpleGrid>
+          <List>
+            <ListItem>
+              <Link
+                href="https://api.whatsapp.com/send?phone=6281292931896"
+                target="_blank"
+              >
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoWhatsapp} />}
+                >
+                  +62 812 xxxx xxx
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
 
-        <Box align="center" my={4}>
-          <NextLink href="/posts" scroll={false}>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              Popular posts
-            </Button>
-          </NextLink>
-        </Box>
-      </Section>
-    </Container>
-  </Layout>
-)
+          <List>
+            <ListItem>
+              <Link href="https://www.tiktok.com/@ytaccount70" target="_blank">
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoTiktok} />}
+                >
+                  Tiktok
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+
+          <List>
+            <ListItem>
+              <Link
+                href="https://www.playstation.com/en-us/ps5/"
+                target="_blank"
+              >
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoPlaystation} />}
+                >
+                  Playstation
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+
+          <List>
+            <ListItem>
+              <Link
+                href="https://id.pinterest.com/ichacnakval/_saved/"
+                target="_blank"
+              >
+                <Button
+                  variant="ghost"
+                  colorScheme="teal"
+                  leftIcon={<Icon as={IoLogoPinterest} />}
+                >
+                  Pinterest
+                </Button>
+              </Link>
+            </ListItem>
+          </List>
+        </Section>
+      </Container>
+    </Layout>
+  )
+}
 
 export default Home
 export { getServerSideProps } from '../components/chakra'
