9e2d677d7a49612e2dc1d3b84fa7642b713046b0
icat108 <ichacnakval@gmail.com>
2022-03-20 10:09:53 +0700

    portfolio

---
+++ components/layouts/main.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import NavBar from '../navbar'
+import NoSsr from '../no-ssr'
+import { Box, Container } from '@chakra-ui/react'
+import VoxelDog from '../voxel-dog'
+
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Ricad Agus Setiawan - Homepage</title>
+      </Head>
+
+      <NavBar path={router.asPath} />
+      <Container maxW="container.md" pt={14}>
+        <NoSsr>
+          <VoxelDog />
+        </NoSsr>
+        {children}
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
+import Fonts from '../components/fonts'
+import theme from '../lib/theme'
+// import { AnimatePresence } from 'framer-motion'
+const { AnimatePresence } = require('framer-motion');
+// const { motion } = require("framer-motion");
+
+const Website = ({ Component, pageProps, router }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout router={router}>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Fonts />
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
+  )
+}
+export default Website
