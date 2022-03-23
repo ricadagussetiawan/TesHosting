91bc3fe65ec58c815d64678cdb51969c7fde374c
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-03-23 16:31:48 +0700

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -10,8 +10,8 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="favicon" href="favicon.icon" />
-            <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="city" href="city.ico" />
+            <link rel="shortcut icon" href="/city.ico" type="image/x-icon" />
         <title>Ricad Agus Setiawan - Homepage</title>
       </Head>
 
